b1aff9d1556bf72208a28d108baf042ac387816e
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-06-11 19:06:49 +0900

    Time: 2 ms (8.02%), Space: 45 MB (87.62%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -2,14 +2,40 @@ import java.util.*;
 
 class Solution {
     public int countNegatives(int[][] grid) {
-        // 음수의 개수
+        // 음수의 개수 return
         int answer = 0;
         
-        for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[i].length; j++) {
-                if(grid[i][j] < 0) answer++;
-            }
+        // 배열 내림차순 정렬 (오름차순 정렬 후 left와 right를 바꾸는 형식)
+        for (int i = 0; i < grid.length; i++) {
+            Arrays.sort(grid[i]);
+            reverseArray(grid[i]);
         }
+        
+        // 내림차순 정렬되어 있으므로 음수를 만나면 이후 원소도 모두 음수
+        for (int i=0; i<grid.length; i++) {
+            int rowLength = grid[i].length; // 각 행의 길이
+            int col = rowLength - 1;    // 제일 끝 원소
+            
+            while (col >= 0 && grid[i][col] < 0) col--;
+            
+            // col 이후는 모두 음수
+            answer += rowLength - (col+1);
+        }
+        
         return answer;
+    }
+    
+    public static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left < right) {
+            int tmp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = tmp;
+            
+            left++;
+            right--;
+        }
     }
 }
